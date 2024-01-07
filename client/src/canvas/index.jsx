ac3f1e405a0b3a4cad5236b18d92057012dcd3af
naveensethupathy <naveensethupathy1325@gmail.com>
2024-01-07 11:24:31 +0530

    dashboard completed

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
 
-const index = () => {
+const CanvasModel = () => {
   return (
     <div>index</div>
   )
 }
 
-export default index
+export default CanvasModel
