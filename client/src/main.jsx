132f29b06cf9553024082e8ac9a1cc5cd28c69b3
naveensethupathy <naveensethupathy1325@gmail.com>
2024-01-14 17:20:40 +0530

    some more changes after deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HashRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:"/Crosx/",
+    element:<App/>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <HashRouter>
+   {/* <HashRouter>
     <App />
-    </HashRouter>  
+    </HashRouter>   */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
